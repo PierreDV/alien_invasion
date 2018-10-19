806d15f4b8a4e9b67633bd91cb204382d7528825
PierreDV <p.verteuil@gmail.com>
2018-10-19 17:45:12 +0200

    refactor keyup and keydown event handlers and move them into their own functions

--- game_functions.py
+++ game_functions.py
@@ -4,20 +4,26 @@ import pygame
 def check_events(ship):
 	"""Respond to keypress and mouse events."""
 	for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				sys.exit()
+		if event.type == pygame.QUIT:
+			sys.exit()
+		elif event.type == pygame.KEYDOWN:
+			check_keydown_events(event, ship)
+		elif event.type == pygame.KEYUP:
+			check_keyup_events(event, ship)
+				
+def check_keydown_events(event, ship):
+	"""Respond to keypresses."""
+	if event.key == pygame.K_RIGHT:
+		ship.moving_right = True
+	elif event.key == pygame.K_LEFT:
+		ship.moving_left = True
 
-			elif event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_RIGHT:
-					ship.moving_right = True
-				elif event.key == pygame.K_LEFT:
-					ship.moving_left = True
-
-			elif event.type == pygame.KEYUP:
-				if event.key == pygame.K_RIGHT:
-					ship.moving_right = False
-				elif event.key == pygame.K_LEFT:
-					ship.moving_left = False
+def check_keyup_events(event, ship):
+	"""Respond to key releases."""
+	if event.key == pygame.K_RIGHT:
+		ship.moving_right = False
+	elif event.key == pygame.K_LEFT:
+		ship.moving_left = False
 
 def update_screen(ai_settings, screen, ship):
 	"""Update images on the screen and flip to the new screen."""
