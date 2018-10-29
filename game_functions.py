d54cc12d234f5db610062dded4a363fc676169cf
PierreDV <p.verteuil@gmail.com>
2018-10-29 20:02:26 +0100

    add logic to move alien fleet to right of screen

--- game_functions.py
+++ game_functions.py
@@ -54,6 +54,10 @@ def update_bullets(bullets):
 		if bullet.rect.bottom <= 0:
 			bullets.remove(bullet)
 
+def update_aliens(aliens):
+	"""Update the positions of all aliens in the fleet."""
+	aliens.update()
+
 def fire_bullet(ai_settings, screen, ship, bullets):
 	"""Fire a bullet if limit not reached yet."""
 	# Create a new bullet and add it to the bullets group.
